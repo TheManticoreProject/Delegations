157b0d97bbb7cd3e2b3f25271546c858390e0d81
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-21 08:26:41 +0200

    Created function AddConstrainedDelegationWithProtocolTransition() in mode add

--- core/mode_add/ConstrainedDelegationsWithProtocolTransition.go
+++ core/mode_add/ConstrainedDelegationsWithProtocolTransition.go
@@ -1,11 +1,73 @@
 package mode_add
 
 import (
+	"fmt"
+	"slices"
+
+	"github.com/TheManticoreProject/Manticore/logger"
+	"github.com/TheManticoreProject/Manticore/network/ldap"
 	"github.com/TheManticoreProject/Manticore/windows/credentials"
 )
 
+// AddConstrainedDelegationWithProtocolTransition adds a constrained delegation with protocol transition to a user or computer account.
+//
+//	Parameters:
+//		ldapHost (string): The LDAP host to connect to.
+//		ldapPort (int): The LDAP port to connect to.
+//		creds (*credentials.Credentials): The credentials to use for the LDAP connection.
+//		useLdaps (bool): Whether to use LDAPS for the LDAP connection.
+//		useKerberos (bool): Whether to use Kerberos for the LDAP connection.
+//		distinguishedName (string): The distinguished name of the user or computer account to add the constrained delegation with protocol transition to.
+//		allowedToDelegateTo ([]string): The list of users or computers that the account is allowed to delegate to.
+//
+//	Returns:
+//		error: An error if the operation fails, nil otherwise.
 func AddConstrainedDelegationWithProtocolTransition(ldapHost string, ldapPort int, creds *credentials.Credentials, useLdaps bool, useKerberos bool, distinguishedName string, allowedToDelegateTo []string) error {
+	ldapSession := ldap.Session{}
+	ldapSession.InitSession(ldapHost, ldapPort, creds, useLdaps, useKerberos)
+	success, err := ldapSession.Connect()
+	if !success {
+		return fmt.Errorf("error connecting to LDAP: %s", err)
+	}
+
+	searchQuery := fmt.Sprintf("(distinguishedName=%s)", distinguishedName)
+	searchAttributes := []string{"msDS-AllowedToDelegateTo", "userAccountControl"}
+	searchResults, err := ldapSession.QueryWholeSubtree("", searchQuery, searchAttributes)
+	if err != nil {
+		return fmt.Errorf("error querying msDS-AllowedToDelegateTo: %s", err)
+	}
+
+	// Check if the object exists
+	if len(searchResults) == 0 {
+		return fmt.Errorf("could not find an object with distinguished name: %s", distinguishedName)
+	}
+
+	// Activate protocol transition (TRUSTED_TO_AUTH_FOR_DELEGATION flag)
+	AddProtocolTransition(ldapHost, ldapPort, creds, useLdaps, useKerberos, distinguishedName)
+
+	// Add constrained delegation
+	if len(searchResults) > 0 {
+		values := searchResults[0].GetEqualFoldAttributeValues("msDS-AllowedToDelegateTo")
+		for _, value := range allowedToDelegateTo {
+			if !slices.Contains(values, value) {
+				values = append(values, value)
+			} else {
+				logger.Info(fmt.Sprintf("Value %s is already present in msDS-AllowedToDelegateTo, not adding it again", value))
+			}
+		}
+
+		err = ldapSession.OverwriteAttributeValues(distinguishedName, "msDS-AllowedToDelegateTo", values)
+		if err != nil {
+			return fmt.Errorf("error adding constrained delegation of %s to %s: %s", distinguishedName, allowedToDelegateTo, err)
+		}
+
+		logger.Info(fmt.Sprintf("Constrained delegation added for %s", distinguishedName))
+
+	} else {
+		return fmt.Errorf("could not find an object with distinguished name: %s", distinguishedName)
+	}
+
+	ldapSession.Close()
 
 	return nil
-
 }
